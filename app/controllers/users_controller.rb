66d6615bf0088b795ec21649a4b72f2108ea8820
hiroyuki33 <delta1887rai@gmail.com>
2019-11-16 23:49:12 +0900

    Finish ch13

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -8,7 +8,8 @@ class UsersController < ApplicationController
   end
 
   def show
-    @user = User.find(params[:id])
+    @user       = User.find(params[:id])
+    @microposts = @user.microposts.paginate(page: params[:page])
   end
 
   def new
@@ -49,35 +50,18 @@ class UsersController < ApplicationController
   private
 
     def user_params
-      params.require(:user).permit(
-        :name, :email, :password, 
-        :password_confirmation)
+      params.require(:user).permit(:name, :email, :password,
+                                   :password_confirmation)
     end
 
-    # ログイン済みユーザーかどうか確認
-    def logged_in_user
-      unless logged_in?
-        flash[:danger] = "Please log in."
-        redirect_to login_url
-      end
-    end
 
-    # ログイン済みユーザーかどうか確認
-    def logged_in_user
-      unless logged_in?
-        store_location
-        flash[:danger] = "Please log in."
-        redirect_to login_url
-      end
-    end
-
-    # 正しいユーザーかどうか確認
+  # 正しいユーザーかどうかを確認
     def correct_user
       @user = User.find(params[:id])
       redirect_to(root_url) unless current_user?(@user)
     end
 
-    # 管理者かどうか確認
+  # 管理者かどうかを確認
     def admin_user
       redirect_to(root_url) unless current_user.admin?
     end
